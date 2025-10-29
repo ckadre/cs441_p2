8d782924284b0b3babbf90fb56f940cfb85c6d62
ckadre <cameronkadre@u.boisestate.edu>
2025-10-29 13:20:46 -0600

    10.29

---
+++ sim.java
@@ -0,0 +1,5 @@
+/**
+ * Reads in memory file, execute the program contained in the memory file, and output info
+ * output: instruction trace, instruction mix, execution time in cycles, number of stalls inserted, 
+ *      number of times forwarding was used (optional, if implemented)
+ */
